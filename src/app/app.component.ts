cc7cea4928fc6ac4fa660ade4f02f0768faaf2dd
Diego <diegusnueva@gmail.com>
2023-07-02 18:20:35 +0200

    Clase finalizada de iniciación a Typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,26 @@ let variable = '1';
 @Component({
   selector: 'app-root',
   template: `
-  
-  <h1> 
-    ${variable}
-    {{ title }} 
-  </h1>
-
+    <h1>
+      ${variable}
+      {{ title }}
+    </h1>
   `,
   //templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title: any;
 
   constructor() {
-    setInterval(() => variable = '2', 1000);
-    setInterval(() => this.title = Math.random(), 1000);
-  }
+
+   
+this.fun(undefined);
+    
+}
+
+fun(x='defecto...'){
+  
+  console.log(x);
+}
 }
