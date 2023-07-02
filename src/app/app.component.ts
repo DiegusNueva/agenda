06eb8e3a036557fdd97d074f0db86757788e6261
Diego <diegusnueva@gmail.com>
2023-07-02 17:36:54 +0200

    Templates literales de EC6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+let variable = '1';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  
+  <h1> 
+    ${variable}
+    {{ title }} 
+  </h1>
+
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!!!!!!!';
+  title: any;
+
+  constructor() {
+    setInterval(() => variable = '2', 1000);
+    setInterval(() => this.title = Math.random(), 1000);
+  }
 }
